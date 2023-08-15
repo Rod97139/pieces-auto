f05e99b81a223689b6352b3b42885956de3dcb54
Rod97139 <fayaflame@gmail.com>
2023-08-15 17:38:06 +0200

    add typescript

--- avis.js
+++ avis.js
@@ -1,144 +1,124 @@
+"use strict";
 /* global Chart */
-
-export const ajoutListenersAvis = () => {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.afficherGraphiqueAvis = exports.ajoutListenerEnvoyerAvis = exports.afficherAvis = exports.ajoutListenersAvis = void 0;
+const ajoutListenersAvis = () => {
     const piecesElements = document.querySelectorAll(".fiches article button");
-
     for (let i = 0; i < piecesElements.length; i++) {
-      piecesElements[i].addEventListener("click", async (event) => {
-        const id = event.target.dataset.id;
-
-        //tester si les avis sont déja present dans le localStorage
+        piecesElements[i].addEventListener("click", (event) => __awaiter(void 0, void 0, void 0, function* () {
+            const id = event.target.dataset.id;
+            //tester si les avis sont déja present dans le localStorage
             let avis = window.localStorage.getItem(`avis-piece-${id}`);
             if (avis === null) {
-              /* Code de récupération des pièces depuis l’API HTTP */
-              avis = await fetch("http://localhost:8081/pieces/" + id + "/avis").then(avis => avis.json());
-
-              // Transformation des pièces en JSON
-              const valeurAvis = JSON.stringify(avis);
-              // Stockage des informations dans le localStorage
-              window.localStorage.setItem(`avis-piece-${id}`, valeurAvis);
-            }else{
+                /* Code de récupération des pièces depuis l’API HTTP */
+                avis = yield fetch("http://localhost:8081/pieces/" + id + "/avis").then(avis => avis.json());
+                // Transformation des pièces en JSON
+                const valeurAvis = JSON.stringify(avis);
+                // Stockage des informations dans le localStorage
+                window.localStorage.setItem(`avis-piece-${id}`, valeurAvis);
+            }
+            else {
                 avis = JSON.parse(avis);
             }
-
-
-        const pieceElement = event.target.parentElement;
-
-        afficherAvis(pieceElement, avis)
-
-        
-      });
+            const pieceElement = event.target.parentElement;
+            (0, exports.afficherAvis)(pieceElement, avis);
+        }));
     }
-}
-
-export const afficherAvis = (pieceElement, avis) => {
+};
+exports.ajoutListenersAvis = ajoutListenersAvis;
+const afficherAvis = (pieceElement, avis) => {
     const avisElement = document.createElement("p");
     for (let i = 0; i < avis.length; i++) {
         avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
     }
     pieceElement.appendChild(avisElement);
-}
-
-export const ajoutListenerEnvoyerAvis = () => {
-  const formulaireAvis = document.querySelector(".formulaire-avis");
-  formulaireAvis.addEventListener("submit", (event) => {
-
-    event.preventDefault();
-
-    // Création de l’objet du nouvel avis.
-    const avis = {
-      pieceId: parseInt(event.target.querySelector("input[name=piece-id]").value),
-      utilisateur: event.target.querySelector("input[name=utilisateur").value,
-      commentaire: event.target.querySelector("input[name=commentaire]").value,
-      nbEtoiles: parseInt(event.target.querySelector("input[name=nbEtoiles]").value)
-    }
-
-    const chargeUtile = JSON.stringify(avis);
-
-    fetch("http://localhost:8081/avis", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: chargeUtile
+};
+exports.afficherAvis = afficherAvis;
+const ajoutListenerEnvoyerAvis = () => {
+    const formulaireAvis = document.querySelector(".formulaire-avis");
+    formulaireAvis.addEventListener("submit", (event) => {
+        event.preventDefault();
+        // Création de l’objet du nouvel avis.
+        const avis = {
+            pieceId: parseInt(event.target.querySelector("input[name=piece-id]").value),
+            utilisateur: event.target.querySelector("input[name=utilisateur").value,
+            commentaire: event.target.querySelector("input[name=commentaire]").value,
+            nbEtoiles: parseInt(event.target.querySelector("input[name=nbEtoiles]").value)
+        };
+        const chargeUtile = JSON.stringify(avis);
+        fetch("http://localhost:8081/avis", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile
+        });
     });
-
-  });
-}
-
-export const afficherGraphiqueAvis = async () => {
-      // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
-      const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
-      const nb_commentaires = [0, 0, 0, 0, 0];
-      for (let commentaire of avis) {
+};
+exports.ajoutListenerEnvoyerAvis = ajoutListenerEnvoyerAvis;
+const afficherGraphiqueAvis = () => __awaiter(void 0, void 0, void 0, function* () {
+    // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
+    const avis = yield fetch("http://localhost:8081/avis").then(avis => avis.json());
+    const nb_commentaires = [0, 0, 0, 0, 0];
+    for (let commentaire of avis) {
         nb_commentaires[commentaire.nbEtoiles - 1]++;
-      }
-
-      const labels = ["5", "4", "3", "2", "1"];
-
-      const data = {
+    }
+    const labels = ["5", "4", "3", "2", "1"];
+    const data = {
         labels: labels,
         datasets: [{
-        label: "Étoiles attribuées",
-        data: nb_commentaires.reverse(),
-          backgroundColor: "rgb(238, 0, 0)", // Couleur de fond
-        }],
+                label: "Étoiles attribuées",
+                data: nb_commentaires.reverse(),
+                backgroundColor: "rgb(238, 0, 0)", // Couleur de fond
+            }],
     };
-
     // Objet de configuration final
     const config = {
-      type: "bar",
-      data: data,
-      options: {
-        indexAxis: "y",
-      },
+        type: "bar",
+        data: data,
+        options: {
+            indexAxis: "y",
+        },
     };
-
     // Rendu du graphique dans l'élément canvas
-    new Chart(
-      document.querySelector("#graphique-avis"),
-      config,
-    );
-
+    new Chart(document.querySelector("#graphique-avis"), config);
     // Recuperation des pieces depuis le localStorage
     const piecesJSON = window.localStorage.getItem("pieces");
-
     const pieces = JSON.parse(piecesJSON);
-
     //Calcul du nombre de commentaire 
-
     let nbCommentairesDispo = 0;
     let nbCommentairesNonDispo = 0;
-
     for (let i = 0; i < avis.length; i++) {
-      const piece = pieces.find(piece => piece.id === avis[i].pieceId);
-      
-      if (piece) {
-        if(piece.disponibilite){
-          nbCommentairesDispo++;
-        }else{
-          nbCommentairesNonDispo++;
+        const piece = pieces.find(piece => piece.id === avis[i].pieceId);
+        if (piece) {
+            if (piece.disponibilite) {
+                nbCommentairesDispo++;
+            }
+            else {
+                nbCommentairesNonDispo++;
+            }
         }
-      }
     }
-
     const labelsDispo = ["Disponible", "Non disponible"];
-
     const dataDispo = {
-      labels: labelsDispo,
-      datasets: [{
-        label: "Nombre de commentaires",
-        data: [nbCommentairesDispo, nbCommentairesNonDispo],
-        backgroundColor: "rgb(238, 0, 0)",
-      }],
+        labels: labelsDispo,
+        datasets: [{
+                label: "Nombre de commentaires",
+                data: [nbCommentairesDispo, nbCommentairesNonDispo],
+                backgroundColor: "rgb(238, 0, 0)",
+            }],
     };
-
     const configDispo = {
-      type: "bar",
-      data: dataDispo,
+        type: "bar",
+        data: dataDispo,
     };
-
-    new Chart(
-      document.querySelector("#graphique-dispo"),
-      configDispo,
-    );
-}
-  
+    new Chart(document.querySelector("#graphique-dispo"), configDispo);
+});
+exports.afficherGraphiqueAvis = afficherGraphiqueAvis;
---
+++ pieces.ts
@@ -0,0 +1,198 @@
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis, afficherGraphiqueAvis  } from "./avis.js";
+
+ajoutListenerEnvoyerAvis()
+// Récupération des pièces depuis le fichier JSON
+// const reponse = await fetch("http://localhost:8081/pieces")
+
+// const pieces = await reponse.json()
+ // Récupération des pièces éventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem("pieces");
+
+if (pieces === null) {
+  /* Code de récupération des pièces depuis l’API HTTP */
+  pieces = await fetch("http://localhost:8081/pieces").then(pieces => pieces.json());
+
+   // Transformation des pièces en JSON
+   const valeurPieces = JSON.stringify(pieces);
+   // Stockage des informations dans le localStorage
+   window.localStorage.setItem("pieces", valeurPieces);
+}else{
+    pieces = JSON.parse(pieces);
+ }
+
+
+const sectionFiches = document.querySelector(".fiches");
+// Fonction qui génère toute la page web
+const genererPieces = (pieces) => {
+  for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i];
+    // Création d’une balise dédiée à une pièce auto
+    const pieceElement = document.createElement("article");
+    pieceElement.dataset.id = pieces[i].id
+
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+     const avisBouton = document.createElement("button");
+     avisBouton.dataset.id = article.id;
+     avisBouton.textContent = "Afficher les avis";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+
+    
+    pieceElement.appendChild(avisBouton);
+    
+}
+ajoutListenersAvis();
+ 
+}
+ 
+// Premier affichage de la page
+genererPieces(pieces);
+
+
+
+for (let i = 0; i < pieces.length; i++) {
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+    console.log(avis);
+
+    if (avis !== null) {
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement, avis);
+    }
+    
+}
+ 
+// Ajout du listener pour trier les pièces par ordre de prix croissant
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", () => {
+   const piecesOrdonnees = Array.from(pieces)
+   piecesOrdonnees.sort((a, b) => {
+       return a.prix - b.prix;
+   });
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesOrdonnees);
+});
+ 
+// Ajout du listener pour filtrer les pièces non abordables
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+boutonFiltrer.addEventListener("click", () => {
+   const piecesFiltrees = pieces.filter((piece) => {
+       return piece.prix <= 35;
+   });
+   // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesFiltrees);
+});
+
+
+//Tri decroissant
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+
+boutonDecroissant.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort( (a, b) => {
+        return b.prix - a.prix;
+     });
+    // Effacement de l'écran et regénération de la page
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesOrdonnees);
+});
+
+//Filtre par catégorie
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+
+boutonNoDescription.addEventListener("click", () => {
+    const piecesFiltrees = pieces.filter( (piece) => {
+        return piece.description
+    });
+    // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
+});
+
+// Tableau des noms des pièces
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+console.log(noms)
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement);
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
+
+//Code Exercice 
+const nomsDisponibles = pieces.map(piece => piece.nom);
+const prixDisponibles = pieces.map(piece => piece.prix);
+
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice(i,1);
+        prixDisponibles.splice(i,1);
+    }
+}
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i=0 ; i < nomsDisponibles.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+    disponiblesElement.appendChild(nomElement);
+}
+
+document.querySelector('.disponibles').appendChild(disponiblesElement);
+
+const inputPrixMax = document.querySelector('#prix-max')
+inputPrixMax.addEventListener('input', () =>{
+    const piecesFiltrees = pieces.filter((piece) =>{
+        return piece.prix <= inputPrixMax.value;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);  
+})
+
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", () => {
+  window.localStorage.removeItem("pieces");
+  for (let i = 0; i < pieces.length; i++) {
+    window.localStorage.removeItem("avis-piece-"+ pieces[i].id);
+  }
+  
+});
+
+afficherGraphiqueAvis();
